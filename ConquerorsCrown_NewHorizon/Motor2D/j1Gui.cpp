11c97a27145df80cd49c2ee40c50b45d7b208f61
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2020-04-20 10:18:01 +0200

    Fixed guiElement parent delete

--- ConquerorsCrown_NewHorizon/Motor2D/j1Gui.cpp
+++ ConquerorsCrown_NewHorizon/Motor2D/j1Gui.cpp
@@ -197,19 +197,14 @@ void j1Gui::DeleteGuiElement()
 {
 	for (int i = 0; i < guiElements.size(); i++)
 	{
+		if (guiElements[i]->parent != nullptr)
+		{
+			if (guiElements[i]->parent->to_delete == true)
+				guiElements.erase(guiElements.begin() + i);
+		}
 		if (guiElements[i]->to_delete == true)
 		{
 			guiElements.erase(guiElements.begin() + i);
-		}
-		else if (guiElements[i]->parent != nullptr)
-		{
-			if (guiElements[i]->parent->to_delete == true)
-				for (int j = 0; j < guiElements.size(); j++) {
-					if (guiElements[j] == guiElements[i]->parent)
-					{
-						guiElements.erase(guiElements.begin() + j);
-					}
-				}
 		}
 	}
 }
