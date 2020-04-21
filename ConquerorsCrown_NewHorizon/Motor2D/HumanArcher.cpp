0b67c7b0d539d8c5e6d22cb3a6607bbbff9c136f
Ap011y0n <47557365+Ap011y0n@users.noreply.github.com>
2020-04-22 00:01:12 +0200

    new selection circles

--- ConquerorsCrown_NewHorizon/Motor2D/HumanArcher.cpp
+++ ConquerorsCrown_NewHorizon/Motor2D/HumanArcher.cpp
@@ -134,8 +134,10 @@ bool HumanArcher::Update(float dt)
 	
 	//App->render->DrawQuad({ (int)position.x, (int)position.y, 10, 10 }, 200, 200, 0);
 	SDL_Rect* r = &current_animation->GetCurrentFrame(dt);
-	//if (isSelected)
+	if (isSelected)
+	App->render->Blit(App->entity->ally_sel_tex, (int)(position.x - 20), (int)(position.y) - 10);
 	//	App->render->DrawCircle((int)position.x, (int)position.y, 20, 0, 200, 0, 200);
+
 	App->render->Blit(App->entity->arch_man_tex, (int)(position.x - (*r).w / 2), (int)(position.y - (*r).h / 2), r, 1.0f, 1.0f, orientation);
 	return true;
 }
