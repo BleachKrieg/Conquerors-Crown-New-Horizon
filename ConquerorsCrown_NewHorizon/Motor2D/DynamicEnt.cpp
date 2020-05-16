0a1a31922a75edccebc7530283a6a680129c2a87
polf780 <47743753+polf780@users.noreply.github.com>
2020-05-16 19:11:19 +0200

    Revert "Revert "Merge branch 'Tutorial' of https://github.com/BleachKrieg/Conquerors-Crown-New-Horizon into Tutorial""
    This reverts commit 6e6b650ff2ad7afea2e54e90d381d5772f207f25.

--- ConquerorsCrown_NewHorizon/Motor2D/DynamicEnt.cpp
+++ ConquerorsCrown_NewHorizon/Motor2D/DynamicEnt.cpp
@@ -186,13 +186,21 @@ void DynamicEnt::AttackTarget(DynamicEntityType type)
 							App->entity->max_audio_attacks++;
 
 						}
-						
 						if (entity_type == DynamicEntityType::ENEMY_TROLL) {
-							SpatialAudio(4, App->audio->troll_attack, position.x, position.y);
+							SpatialAudio(5, App->audio->troll_attack, position.x, position.y);
 							App->entity->max_audio_attacks++;
 
 						}
-						
+						if (entity_type == DynamicEntityType::ENEMY_GRUNT) {
+							SpatialAudio(6, App->audio->grunt_attack, position.x, position.y);
+							App->entity->max_audio_attacks++;
+
+						}
+						if (entity_type == DynamicEntityType::ENEMY_OGRE) {
+							SpatialAudio(7, App->audio->ogre_attack, position.x, position.y);
+							App->entity->max_audio_attacks++;
+
+						}
 					}
 					
 				}
@@ -464,18 +472,24 @@ void DynamicEnt::Death(DynamicEntityType entity_type)
 	death_counter++;
 	if (death_counter == 2) {
 		if (entity_type == DynamicEntityType::HUMAN_ARCHER) {
-			SpatialAudio(6, App->audio->die_archer, position.x, position.y);
+			SpatialAudio(8, App->audio->die_archer, position.x, position.y);
 
 		}
 		if (entity_type == DynamicEntityType::HUMAN_FOOTMAN) {
-			SpatialAudio(7, App->audio->die_footman, position.x, position.y);
+			SpatialAudio(8, App->audio->die_footman, position.x, position.y);
 		}
-		/*if (entity_type == DynamicEntityType::HUMAN_GATHERER) {
+		if (entity_type == DynamicEntityType::HUMAN_GATHERER) {
 			SpatialAudio(8, App->audio->die_gatherer, position.x, position.y);
-		}*/
+		}
 		if (entity_type == DynamicEntityType::ENEMY_TROLL) {
-			SpatialAudio(9, App->audio->die_troll, position.x, position.y);
-		}		
+			SpatialAudio(8, App->audio->die_troll, position.x, position.y);
+		}	
+		if (entity_type == DynamicEntityType::ENEMY_GRUNT) {
+			SpatialAudio(8, App->audio->die_grunt, position.x, position.y);
+		}
+		if (entity_type == DynamicEntityType::ENEMY_OGRE) {
+			SpatialAudio(8, App->audio->die_ogre, position.x, position.y);
+		}
 	}
 //	LOG("DEATH: %i", death_counter);
 	if (current_animation->Finished() == true) { to_delete = true; }
