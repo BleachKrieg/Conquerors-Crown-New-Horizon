9c7ec40c6db7108510ac06bc6de132221e03b247
pauraurell <47743764+pauraurell@users.noreply.github.com>
2020-05-26 22:27:30 +0200

    Knight 2nd spritesheet

--- ConquerorsCrown_NewHorizon/Motor2D/j1EntityManager.h
+++ ConquerorsCrown_NewHorizon/Motor2D/j1EntityManager.h
@@ -87,6 +87,7 @@ public:
 	SDL_Texture* arch_man_tex2 = nullptr;
 	SDL_Texture* gather_man_tex = nullptr;
 	SDL_Texture* knight_tex = nullptr;
+	SDL_Texture* knight_tex2 = nullptr;
 	SDL_Texture* troll_tex = nullptr;
 	SDL_Texture* ally_sel_tex = nullptr;
 	SDL_Texture* enemy_sel_tex = nullptr;
